458112a38b5984eaab1c081f0d4593973799b9d0
Nel Nibcord <blindcrone@tuta.io>
2024-11-04 03:44:30 -0800

    Okay let's try this

--- exo/main.py
+++ exo/main.py
@@ -225,7 +225,7 @@ async def eval_model_cli(node: Node, inference_engine: InferenceEngine, model_na
     batch_losses = []
     batch_toks = []
     for example, target, length in zip(*batch):
-      losses, toks = await node.evaluate(shard, ins, outs, lens)
+      losses, toks = await node.evaluate(shard, example, target, length)
       if losses is not None:
         batch_losses.append(losses)
         batch_toks.append(toks)
