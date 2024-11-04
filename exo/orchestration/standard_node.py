a7bba3e663af5c83d028f3cf63f9a6b499a0ea7f
Nel Nibcord <blindcrone@tuta.io>
2024-11-04 02:09:43 -0800

    Okay let's try this

--- exo/orchestration/standard_node.py
+++ exo/orchestration/standard_node.py
@@ -210,6 +210,7 @@ class StandardNode(Node):
     _, _, _ = await callback.wait(lambda _request_id, tokens, is_finished: _request_id == example_id and is_finished, timeout=300)
     if(shard.is_last_layer()):
       output: np.ndarray = np.array(self.buffered_raw_output[example_id][0]) 
+      print(output.shape, targets.shape)
       return self.inference_engine.eval_metric(output, targets, length[0])
     else: 
       return None, None
