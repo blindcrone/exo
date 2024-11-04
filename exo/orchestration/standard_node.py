43c18c73b177a47124aaafda05ab8c0de835ca08
Nel Nibcord <blindcrone@tuta.io>
2024-11-04 03:20:52 -0800

    Okay let's try this

--- exo/orchestration/standard_node.py
+++ exo/orchestration/standard_node.py
@@ -209,7 +209,7 @@ class StandardNode(Node):
     resp = await self.process_tensor(base_shard, inputs[0], example_id)
     _, _, _ = await callback.wait(lambda _request_id, tokens, is_finished: _request_id == example_id and is_finished, timeout=300)
     if(shard.is_last_layer()):
-      raw: np.ndarray = np.array(self.buffered_raw_output[example_id][0])
+      raw: np.ndarray = np.array(self.buffered_raw_output[example_id][0][:length[0])
       print(raw[0].shape, raw[1].shape)
       return self.inference_engine.eval_metric(raw, targets, length[0])
     else: 
