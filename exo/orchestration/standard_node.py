983000417f2eac2320042d21de7115c263154cff
Nel Nibcord <blindcrone@tuta.io>
2024-11-04 03:18:19 -0800

    Okay let's try this

--- exo/orchestration/standard_node.py
+++ exo/orchestration/standard_node.py
@@ -119,7 +119,7 @@ class StandardNode(Node):
       self.buffered_raw_output[request_id] = ([], False)
 
     self.buffered_raw_output[request_id][0].append(result.squeeze())
-    print(result.shape)
+    print(self.buffered_raw_output[request_id][0].shape)
 
     if shard.is_last_layer():
       result = await self.inference_engine.sample(result)
