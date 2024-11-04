331577ecde09b83ccc0b74f9193afa136e8ebbfa
Nel Nibcord <blindcrone@tuta.io>
2024-11-04 01:39:23 -0800

    Okay let's try this

--- exo/inference/mlx/sharded_inference_engine.py
+++ exo/inference/mlx/sharded_inference_engine.py
@@ -26,7 +26,8 @@ class MLXDynamicShardInferenceEngine(InferenceEngine):
     self.executor = ThreadPoolExecutor(max_workers=1)
 
   def eval_metric(self, outputs, targets, lengths):
-    x = mx.array(outputs)
+    x = mx.array(outputs[:lengths])
+    y = x
     y = mx.array(targets)
     l = mx.array([lengths])
     return masked_ce_from_logits(x, y, l)
