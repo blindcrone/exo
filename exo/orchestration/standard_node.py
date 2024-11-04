fc41b240e218b98be963b0c436fc47080edfd976
Nel Nibcord <blindcrone@tuta.io>
2024-11-04 02:03:06 -0800

    Okay let's try this

--- exo/orchestration/standard_node.py
+++ exo/orchestration/standard_node.py
@@ -206,12 +206,11 @@ class StandardNode(Node):
     shard = self.get_current_shard(base_shard)
     example_id = str(uuid.uuid4())
     callback = self.on_token.register("eval-wait-{callback_id}")
-    print(inputs.shape, targets.shape, length)
     resp = await self.process_tensor(base_shard, inputs[0], example_id)
     _, _, _ = await callback.wait(lambda _request_id, tokens, is_finished: _request_id == example_id and is_finished, timeout=300)
     if(shard.is_last_layer()):
       output: np.ndarray = np.array(self.buffered_raw_output[example_id][0]) 
-      return self.inference_engine.eval_metric(output, targets, length)
+      return self.inference_engine.eval_metric(output, targets, length.item())
     else: 
       return None, None
         
