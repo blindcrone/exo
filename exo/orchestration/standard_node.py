99099a0feb1f5bf4e2de437959723596857fb9d5
Nel Nibcord <blindcrone@tuta.io>
2024-11-04 01:28:16 -0800

    Okay let's try this

--- exo/orchestration/standard_node.py
+++ exo/orchestration/standard_node.py
@@ -210,7 +210,7 @@ class StandardNode(Node):
     for inp, target, length in zip(*batch):
       example_id = str(uuid.uuid4())
       callback = self.on_token.register("eval-wait-{callback_id}")
-      self.process_tensor(base_shard, inp, example_id)
+      resp = await self.process_tensor(base_shard, inp, example_id)
       _, _, _ = await callback.wait(lambda _request_id, tokens, is_finished: _request_id == example_id and is_finished, timeout=300)
       if(shard.is_last_layer()):
         losses.append(self.inference_engine.eval_metric(self.buffered_raw_output[example_id][0], target, length))
