82274af67cf192ed69ed47327f1f597ca7748e9d
Nel Nibcord <blindcrone@tuta.io>
2024-11-06 04:04:34 -0800

    Jacked

--- exo/inference/mlx/sharded_model.py
+++ exo/inference/mlx/sharded_model.py
@@ -21,7 +21,6 @@ class StatefulShardedModel:
     x,
     request_id: str,
   ) -> Generator[Tuple[mx.array, mx.array], None, None]:
-    y = x
     if request_id not in self.caches:
       self.init_cache(request_id)
     else:
@@ -29,7 +28,7 @@ class StatefulShardedModel:
 
     cache = self.caches[request_id]
 
-    output = self.model(y[None] if self.shard.is_first_layer() else y, cache=cache)
+    output = self.model(x[None] if self.shard.is_first_layer() else x, cache=cache)
     return output
 
   def __call__(
